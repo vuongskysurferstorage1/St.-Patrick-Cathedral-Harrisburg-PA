0f20481e2acf59113e8500dc386b8dd77150dbb6
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-08-30 23:50:03 -0400

    360 upload

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-dji_20250830085635_0001_d-360-good",
-      "name": "DJI_20250830085635_0001_D 360 GOOD",
+      "id": "0-st-patrick-cathedral-harrisburg-pa",
+      "name": "St. Patrick Cathedral Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -37,32 +37,32 @@ var APP_DATA = {
           "yaw": 0.009682380156107229,
           "pitch": 0.050537151210228615,
           "rotation": 0,
-          "target": "1-dji_20250830085905_0002_d-360-good"
+          "target": "1-st-patrick-cathedral-harrisburg-pa"
         },
         {
-          "yaw": 0.32586944403422713,
-          "pitch": -0.15199966127088516,
+          "yaw": 0.31450847022150974,
+          "pitch": -0.15215600085004333,
           "rotation": 0,
-          "target": "2-dji_20250830090230_0010_d-360-good"
+          "target": "2-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": -0.3025848231372201,
           "pitch": -0.14551145016949008,
           "rotation": 0,
-          "target": "3-dji_20250830090643_0017_d-360-good"
+          "target": "3-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": 0.02590427915185778,
           "pitch": 0.9018646938734527,
           "rotation": 0,
-          "target": "4-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-5"
+          "target": "4-st-patrick-cathedral-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-dji_20250830085905_0002_d-360-good",
-      "name": "DJI_20250830085905_0002_D 360 GOOD",
+      "id": "1-st-patrick-cathedral-harrisburg-pa",
+      "name": "St. Patrick Cathedral Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -97,26 +97,26 @@ var APP_DATA = {
           "yaw": -3.110045952810161,
           "pitch": 0.1353102027478439,
           "rotation": 0,
-          "target": "0-dji_20250830085635_0001_d-360-good"
+          "target": "0-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": 0.37225392443832916,
           "pitch": -0.12645722542365334,
           "rotation": 0,
-          "target": "2-dji_20250830090230_0010_d-360-good"
+          "target": "2-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": -0.6222089759242664,
           "pitch": -0.09534451036505232,
           "rotation": 0,
-          "target": "3-dji_20250830090643_0017_d-360-good"
+          "target": "3-st-patrick-cathedral-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-dji_20250830090230_0010_d-360-good",
-      "name": "DJI_20250830090230_0010_D 360 GOOD",
+      "id": "2-st-patrick-cathedral-harrisburg-pa",
+      "name": "St. Patrick Cathedral Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -151,20 +151,20 @@ var APP_DATA = {
           "yaw": -0.23077488188136464,
           "pitch": 0.09694813974401129,
           "rotation": 0,
-          "target": "0-dji_20250830085635_0001_d-360-good"
+          "target": "0-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": 1.1096378756833705,
           "pitch": 0.00003731446787824666,
           "rotation": 0,
-          "target": "3-dji_20250830090643_0017_d-360-good"
+          "target": "3-st-patrick-cathedral-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-dji_20250830090643_0017_d-360-good",
-      "name": "DJI_20250830090643_0017_D 360 GOOD",
+      "id": "3-st-patrick-cathedral-harrisburg-pa",
+      "name": "St. Patrick Cathedral Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -199,20 +199,20 @@ var APP_DATA = {
           "yaw": 0.10121504630765088,
           "pitch": 0.10292614930303046,
           "rotation": 0,
-          "target": "0-dji_20250830085635_0001_d-360-good"
+          "target": "0-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": -0.927769229033828,
           "pitch": -0.027025403106996748,
           "rotation": 0,
-          "target": "2-dji_20250830090230_0010_d-360-good"
+          "target": "2-st-patrick-cathedral-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-5",
-      "name": "360 View of St. Patrick Cathedral  Harrisburg, PA Interior 5",
+      "id": "4-st-patrick-cathedral-harrisburg-pa",
+      "name": "St. Patrick Cathedral Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -247,26 +247,32 @@ var APP_DATA = {
           "yaw": -0.018444506729279553,
           "pitch": 1.2714606388066763,
           "rotation": 0,
-          "target": "5-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-4"
+          "target": "5-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": 0.0032896114678706567,
           "pitch": 0.46073232362236993,
           "rotation": 0,
-          "target": "6-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-1"
+          "target": "6-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": 0.0033333217669344606,
           "pitch": 0,
           "rotation": 0,
-          "target": "8-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-3"
+          "target": "8-st-patrick-cathedral-harrisburg-pa"
+        },
+        {
+          "yaw": -3.122674577496575,
+          "pitch": 1.0411058504453656,
+          "rotation": 0,
+          "target": "0-st-patrick-cathedral-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-4",
-      "name": "360 View of St. Patrick Cathedral  Harrisburg, PA Interior 4",
+      "id": "5-st-patrick-cathedral-harrisburg-pa",
+      "name": "St. Patrick Cathedral Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -298,35 +304,35 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 3.138864828088412,
-          "pitch": -1.1044850860469033,
+          "yaw": 3.1361164617717447,
+          "pitch": -0.7134954503171542,
           "rotation": 0,
-          "target": "4-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-5"
+          "target": "4-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": -0.0033499206188238873,
           "pitch": 0.29426564993657855,
           "rotation": 0,
-          "target": "6-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-1"
+          "target": "6-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": -0.007835567270214838,
           "pitch": -0.5151476046895702,
           "rotation": 0,
-          "target": "9-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-6"
+          "target": "9-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": 3.1336967522519625,
           "pitch": 0.40498762579534286,
           "rotation": 0,
-          "target": "0-dji_20250830085635_0001_d-360-good"
+          "target": "0-st-patrick-cathedral-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "6-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-1",
-      "name": "360 View of St. Patrick Cathedral  Harrisburg, PA Interior 1",
+      "id": "6-st-patrick-cathedral-harrisburg-pa",
+      "name": "St. Patrick Cathedral Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -361,26 +367,26 @@ var APP_DATA = {
           "yaw": 3.137952990437044,
           "pitch": 0.03530353031325717,
           "rotation": 0,
-          "target": "5-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-4"
+          "target": "5-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": 3.136856582633939,
           "pitch": -0.25758042125972125,
           "rotation": 0,
-          "target": "4-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-5"
+          "target": "4-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": 0.1595962873231791,
           "pitch": -1.4526888457447882,
           "rotation": 0,
-          "target": "8-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-3"
+          "target": "8-st-patrick-cathedral-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "7-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-2",
-      "name": "360 View of St. Patrick Cathedral  Harrisburg, PA Interior 2",
+      "id": "7-st-patrick-cathedral-harrisburg-pa",
+      "name": "St. Patrick Cathedral Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -415,26 +421,26 @@ var APP_DATA = {
           "yaw": 0.00408439947416106,
           "pitch": -0.2615386304843934,
           "rotation": 0,
-          "target": "4-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-5"
+          "target": "4-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": 0.004818800324837724,
           "pitch": -0.03464760076685636,
           "rotation": 0,
-          "target": "5-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-4"
+          "target": "5-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": -0.6753229782953234,
           "pitch": -1.552672365635635,
           "rotation": 0,
-          "target": "8-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-3"
+          "target": "8-st-patrick-cathedral-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "8-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-3",
-      "name": "360 View of St. Patrick Cathedral  Harrisburg, PA Interior 3",
+      "id": "8-st-patrick-cathedral-harrisburg-pa",
+      "name": "St. Patrick Cathedral Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -469,32 +475,32 @@ var APP_DATA = {
           "yaw": 0.0015539115127687353,
           "pitch": -0.05790067873901705,
           "rotation": 0,
-          "target": "4-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-5"
+          "target": "4-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": -0.8819624515896471,
           "pitch": -1.5654827552793655,
           "rotation": 0,
-          "target": "9-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-6"
+          "target": "9-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": 0.0033384508145193337,
           "pitch": 0.19001363056006326,
           "rotation": 0,
-          "target": "5-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-4"
+          "target": "5-st-patrick-cathedral-harrisburg-pa"
         },
         {
           "yaw": 2.903536752234957,
           "pitch": 1.5218392530868492,
           "rotation": 0,
-          "target": "7-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-2"
+          "target": "7-st-patrick-cathedral-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "9-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-6",
-      "name": "360 View of St. Patrick Cathedral  Harrisburg, PA Interior 6",
+      "id": "9-st-patrick-cathedral-harrisburg-pa",
+      "name": "St. Patrick Cathedral Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -529,13 +535,13 @@ var APP_DATA = {
           "yaw": -0.010451045753749,
           "pitch": 1.452517498254485,
           "rotation": 0,
-          "target": "7-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-2"
+          "target": "7-st-patrick-cathedral-harrisburg-pa"
         },
         {
-          "yaw": -3.1352791033586644,
-          "pitch": 0.4427959579132015,
+          "yaw": -3.135724217056758,
+          "pitch": 0.43396415402822974,
           "rotation": 0,
-          "target": "5-360-view-of-st-patrick-cathedral--harrisburg-pa-interior-4"
+          "target": "5-st-patrick-cathedral-harrisburg-pa"
         }
       ],
       "infoHotspots": []
